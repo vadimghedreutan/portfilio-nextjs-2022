d7729f924a70ca2b0ba02fb6847e8d56323e7a4b
vadimghedreutan <vadim.ghedreutan@icloud.com>
2022-01-08 21:39:24 +0100

    add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,30 @@
 import '../styles/globals.scss'
 import { ThemeProvider } from 'next-themes'
 import { AnimatePresence } from 'framer-motion'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<ThemeProvider enableSystem={true} attribute="class">
-			<AnimatePresence exitBeforeEnter>
-				<Component {...pageProps} />
-			</AnimatePresence>
-		</ThemeProvider>
+		<>
+			<Script
+				strategy="lazyOnload"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+			/>
+			<Script id="ga-analytics">
+				{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+			</Script>
+			<ThemeProvider enableSystem={true} attribute="class">
+				<AnimatePresence exitBeforeEnter>
+					<Component {...pageProps} />
+				</AnimatePresence>
+			</ThemeProvider>
+		</>
 	)
 }
 
